b5e3787ee14d39de71cdaa5b75c16447e1a4a165
Thomas Planes <tplanes@student.42lausanne.ch>
2023-03-29 12:08:39 +0200

    square white sprite ok

--- src/sprite.c
+++ src/sprite.c
@@ -6,7 +6,7 @@
 /*   By: tplanes <tplanes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 09:56:31 by tplanes           #+#    #+#             */
-/*   Updated: 2023/03/29 11:27:06 by tplanes          ###   ########.fr       */
+/*   Updated: 2023/03/29 12:03:35 by tplanes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,14 @@ static void draw_pix_group(t_image *im, int i, int i_ray, int color);
 void update_sprite(t_player *p, t_sprite *sp, t_meta *meta)
 {
 
-	(void)meta;
+	t_text_map smap;
+	
 	sp->x = 1696;
 	sp->y = 736;
 	sp->dx = sp->x - p->x; 
 	sp->dy = sp->y - p->y;
-	if (fabsf(sp->dx) < 5 || fabsf(sp->dy) < 5)
-		return ;
+	//if (fabsf(sp->dx) < 5 || fabsf(sp->dy) < 5)
+	//	return ;
 		
 	float cost = cosf(p->theta);
 	float sint = sinf(p->theta);
@@ -32,42 +33,58 @@ void update_sprite(t_player *p, t_sprite *sp, t_meta *meta)
 	//float offset = sp->dx * -sint + sp->dy * cost;
 	//int i_ray = N_RAY / 2 + (int)offset;
 	
-	//float theta_s = atanf(sp->dy/sp->dx);
 	float theta_s = atan2f(sp->dy, sp->dx);
 	
 	float dtheta_s;
 	if (theta_s < 0)
 		theta_s = 2 * PI + theta_s;
 	dtheta_s = theta_s - p->theta;
-	if (fabsf(dtheta_s) > FOV / 2)
-		return ;
-	//if (theta_s >= p->theta)
-	//	dtheta_s = theta_s - p->theta;
-	//else
-	//	dtheta_s = 2 * PI + (theta_s - p->theta);
-	//float dtheta_s = fmodf(p -> theta + (2 * PI - p -> d_theta), 2 * PI);
 		
-	int i_ray = (int)(dtheta_s / (FOV / 2) * (float)(N_RAY - 1) / 2) + N_RAY / 2;
+	int i_ray_center = (int)(dtheta_s / (FOV / 2) * (float)(N_RAY - 1) / 2) + N_RAY / 2;
 
-	if (i_ray < 0 || i_ray > N_RAY - 1)
-		return ;
-
-
-//	int sp_size = PIXEL_PER_BLOCK;	
 	int sp_size = 32;	
 	int h = (int)round((float)sp_size / dist * (float)D_P2P);
+	int w = (int)round((float)sp_size / dist * (float)(D_P2P / INTERP_FACT));
 	if (h > IM3_NY)
 		return ;
-	int i = (IM3_NY - h) / 2;
-    int color;
-	while (i < (IM3_NY + h) / 2)
-    {
-        color = WHITE;//*(tmap -> ptr_text + (int)(tmap -> text_offset) * TEXT_SIZE);
-        draw_pix_group(&meta->im, i, i_ray, color);
-        //tmap -> text_offset += tmap -> dtext;
-        i++;
-    }
+	int i;
+   	int color;
 
+	(void)smap;
+	
+	int i_ray = i_ray_center - w / 2;
+	while (i_ray < i_ray_center + w / 2)
+	{
+		
+		if (i_ray < 0 || i_ray > N_RAY - 1)
+		{	
+			i_ray++;
+			continue ;
+		}
+		/*
+		smap.ptr_text = (int *)(meta -> input.textures[4].addr) + ray -> p1.x % PIX_PER_BLOCK;
+
+
+		tmap.dtext = (double)TEXT_SIZE / (double)h;
+     tmap.text_offset = 0.0;
+     tmap.ptr_text = ptr_text;
+     if (h > IM3_NY)
+     {
+         tmap.text_offset = 0.5 * ((double)(h - IM3_NY)) * tmap.dtext;
+         h = IM3_NY;
+     }
+	*/
+	
+		i = (IM3_NY - h) / 2;
+		while (i < (IM3_NY + h) / 2)
+    	{
+       		color = WHITE;//*(tmap -> ptr_text + (int)(tmap -> text_offset) * TEXT_SIZE);
+       		draw_pix_group(&meta->im, i, i_ray, color);
+       		//tmap -> text_offset += tmap -> dtext;
+       		i++;
+    	}
+		i_ray++;
+	}	
 	return ;
 }
 
